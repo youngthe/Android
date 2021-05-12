8cb501c3ddf52e9d9a90aa19b62e5d1bde5a3756
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2021-05-12 14:29:32 +0900

    first commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,235 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Permission;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+        int year = 2020;
+        int month = 3;
+        int date = 9;
+        int totalWork = 640; //총 복무일
+        TextView Current_class; //현재 계급
+        TextView Next_class; //다음 계급
+        TextView textView_endDday;
+        TextView textView_percent1;
+        TextView textView_percent2;
+        TextView textView_nextClassD_day;
+        ProgressBar progressbar1;
+        ProgressBar progressbar2;
+        ImageView Main_background;
+        String imgName = "myapp.png";    // 이미지 이름
+
+    public void onClickModify(View v){
+        Intent intent = new Intent(this, Setting.class);
+        startActivity(intent);
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int howmanyWork = countdday(year,month,date); //얼마나 복무했는가
+        int remainingWork = totalWork - howmanyWork; //얼마 남았는지
+        textView_endDday = findViewById(R.id.endD_day);
+        textView_endDday.setText("D-"+(remainingWork-1)); //얼마 남았는지 D-DAY 출력
+
+        //군 복무 percent 구하는 함수
+        textView_percent1 = findViewById(R.id.percent1);
+        float howmanypercent = ((float)howmanyWork/(float)totalWork)*(float)100;
+        textView_percent1.setText(String.format("%.1f", howmanypercent)+"%");
+        progressbar1 = findViewById(R.id.determinateBar1);
+        progressbar1.setProgress((int)howmanypercent);
+        WhatyourClass(year, month, date);//현재 계급, 다음 계급, 다음계급까지 얼마나 남았는지 출력
+
+        //test
+        Main_background = findViewById(R.id.Main_background);
+        try {
+            String imgpath = getCacheDir() +"/"+ imgName;   // 내부 저장소에 저장되어 있는 이미지 경로
+            Bitmap bm = BitmapFactory.decodeFile(imgpath);
+            Main_background.setImageBitmap(bm);   // 내부 저장소에 저장된 이미지를 이미지뷰에 셋
+        } catch (Exception e) {}
+
+       //배경화면 클릭을 하면 갤러리를 키는 함수
+        Main_background.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+
+    }
+
+
+    // 갤러리를 실행시키고 갤러리에서 가져온 이미지를 비트맵을 사용해서 저장시키는 함수
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { // 갤러리
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                    Main_background.setImageBitmap(imgBitmap);    // 선택한 이미지 이미지뷰에 셋
+                    instream.close();   // 스트림 닫아주기
+                    saveBitmapToJpeg(imgBitmap);    // 내부 저장소에 저장
+                } catch (Exception e) {}
+            }
+        }
+    }
+
+    //위 함수에서 사용하는 이미지 저장함수
+    public void saveBitmapToJpeg(Bitmap bitmap) {   // 선택한 이미지 내부 저장소에 저장
+        File tempFile = new File(getCacheDir(), imgName);    // 파일 경로와 이름 넣기
+        try {
+            tempFile.createNewFile();   // 자동으로 빈 파일을 생성하기
+            FileOutputStream out = new FileOutputStream(tempFile);  // 파일을 쓸 수 있는 스트림을 준비하기
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);   // compress 함수를 사용해 스트림에 비트맵을 저장하기
+            out.close();    // 스트림 닫아주기
+        } catch (Exception e) {}
+    }
+
+//    public void bt2(View view) {    // 이미지 삭제
+//        try {
+//            File file = getCacheDir();  // 내부저장소 캐시 경로를 받아오기
+//            File[] flist = file.listFiles();
+//            for (int i = 0; i < flist.length; i++) {    // 배열의 크기만큼 반복
+//                if (flist[i].getName().equals(imgName)) {   // 삭제하고자 하는 이름과 같은 파일명이 있으면 실행
+//                    flist[i].delete();  // 파일 삭제
+//                    Toast.makeText(getApplicationContext(), "파일 삭제 성공", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        } catch (Exception e) {
+//            Toast.makeText(getApplicationContext(), "파일 삭제 실패", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+
+
+    //얼마나 복무했는지 알려주는 함수
+    public int countdday(int myear, int mmonth, int mdate){
+        try{
+            Calendar todayCal = Calendar.getInstance(); //오늘 날짜 가져오기
+            Calendar ddayCal = Calendar.getInstance(); //오늘 날짜를 가져와 변경시킴
+
+            mmonth -=1;
+            ddayCal.set(myear,mmonth,mdate);
+
+            long today = todayCal.getTimeInMillis()/86400000; //초 변환
+            long date = ddayCal.getTimeInMillis()/86400000;
+            long count = today - date;
+            return (int) count;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    //계급이 무엇인지 나타내주는 함수
+    public void WhatyourClass(int year, int month, int date) {
+        Calendar todayCal = Calendar.getInstance(); //오늘 날짜 가져오기
+        Calendar startDay = Calendar.getInstance();  //입대일
+        Calendar level1 = Calendar.getInstance(); //훈련병 //2020.03.09
+        Calendar level2 = Calendar.getInstance(); //이병   //2020.05.09
+        Calendar level3 = Calendar.getInstance(); //일병   //2020.11.09
+        Calendar level4 = Calendar.getInstance(); //상병   //2021.05.09
+        Calendar level5 = Calendar.getInstance(); //병장   //2021.12.08
+        month=month-1;
+        startDay.set(year, month, date);
+        level1.set(year, month + 1, date); //훈련병(1개월)
+        level2.set(year, month + 2, date); //훈련병(1개월)+이병(2개월)
+        level3.set(year, month + 8, date); //이병(2개월)+일병(6개월)
+        level4.set(year, month + 14, date); //이병(2개월)+일병(6개월)+상병(6개월)
+        level5.set(year, month + 21, date); //이병(2개월)+일병(6개월)+상병(6개월)+병장(7개월)
+
+        long today = todayCal.getTimeInMillis() / 86400000;
+        long start = startDay.getTimeInMillis() / 86400000;
+        long onelevel = level1.getTimeInMillis() / 86400000;//훈련병(1개월)
+        long twolevel = level2.getTimeInMillis() / 86400000;//이병(2개월)
+        long treelevel = level3.getTimeInMillis() / 86400000;//이병(2개월)+일병(6개월)
+        long fourlevel = level4.getTimeInMillis() / 86400000;//이병(2개월)+일병(6개월)+상병(6개월)
+        long fivelevel = level5.getTimeInMillis() / 86400000;//이병(2개월)+일병(6개월)+상병(6개월)+병장(7개월)
+
+        long HowmanyNextClass; //다음 계급까지 얼마나 남았는가
+        long total; //각 계급별 총 근무일
+        float percent; //다음 계급까지 현재 계급에서 복무한 총 일수
+        Current_class = findViewById(R.id.Current_class); //현재 계급
+        Next_class = findViewById(R.id.Next_class); //다음 계급
+        textView_percent2 = findViewById(R.id.percent2);
+        progressbar2 = findViewById(R.id.determinateBar2);
+        //현재 계급, 다음계급, 다음계급까지 남은 일 수 출력
+        if(today < start) {
+            HowmanyNextClass = start-today;
+            Current_class.setText("민간인");
+            Next_class.setText("훈련병");
+            total = start-today;
+            percent = ((float)(today-start)/(float)total)*(float)100;
+        }else if (today < onelevel) {
+            HowmanyNextClass = onelevel-today;
+            Current_class.setText("훈련병");
+            Next_class.setText("이병");
+            total = onelevel-start;
+            percent = ((float)(today-start)/(float)total)*(float)100;
+        }else if (today < twolevel) {
+            HowmanyNextClass = twolevel-today;
+            Current_class.setText("이병");
+            Next_class.setText("일병");
+            total = twolevel-onelevel;
+            percent = ((float)(today-onelevel)/(float)total)*(float)100;
+        }else if (today < treelevel) {
+            HowmanyNextClass = treelevel-today;
+            Current_class.setText("일병");
+            Next_class.setText("상병");
+            total = treelevel-twolevel;
+            percent = ((float)(today-twolevel)/(float)total)*(float)100;
+        }else if (today < fourlevel) {
+            HowmanyNextClass= fourlevel-today;
+            Current_class.setText("상병");
+            Next_class.setText("병장");
+            total = fourlevel-treelevel;
+            percent = ((float)(today-treelevel)/(float)total)*(float)100;
+        }else if (today < fivelevel) {
+            HowmanyNextClass = fivelevel-today;
+            Current_class.setText("병장");
+            Next_class.setText("민간인");
+            total = fivelevel-fourlevel;
+            percent = ((float)(today-fourlevel)/(float)total)*(float)100;
+        }else {
+            HowmanyNextClass = today-fivelevel;
+            Current_class.setText("민간인");
+            Next_class.setText("민간인");
+            percent = 100;
+        }
+        textView_percent2.setText(String.format("%.1f", percent)+"%");
+        progressbar2.setProgress((int)percent);
+        textView_nextClassD_day = findViewById(R.id.nextClassD_day);//다음 계급까지 남은 날
+        textView_nextClassD_day.setText("D-"+(int)(HowmanyNextClass));
+    }
+}
---
+++ app/src/main/java/com/example/myapp/Setting.java
@@ -0,0 +1,191 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.app.DatePickerDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class Setting extends AppCompatActivity {
+    Button bt1;
+    Button bt2;
+    SQLiteDatabase SQLitedb;
+    int ComeYear, ComeMonth, ComeDay;
+    int OutYear, OutMonth, OutDay;
+    ImageView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //DatePickerDialog 객체 dialog1, dialog2 생성
+        DatePickerDialog dialog1 = new DatePickerDialog(this, listener1, 2021, 3, 18);
+        DatePickerDialog dialog2 = new DatePickerDialog(this, listener2, 2021, 3, 18);
+
+        //bt1(입대일) 입력시에 데이트피커 창 오픈
+        bt1 = findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dialog1.show();
+            }
+        });
+
+        //bt2(전역일) 입력시에 데이트피커 창 오픈
+        bt2 = findViewById(R.id.bt2);
+        bt2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dialog2.show();
+            }
+        });
+
+        //데이터베이스 생성.
+        init_database();
+
+        //테이블 생성
+        init_table();
+
+        //값 입력
+        load_values();
+
+        //저장하기 버튼을 누르면 실행
+        Button save_button = findViewById(R.id.set_button2);
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save_values();
+            }
+        });
+    }
+
+    private void load_values(){
+
+        //입대날짜 데이터베이스에서 불러오기
+        try {
+            String SelectComeSQL = "select * from DDay where num=0";
+            String ComeDate;
+            Cursor cursor1 = SQLitedb.rawQuery(SelectComeSQL, null);
+            if(cursor1.moveToFirst()) {
+                ComeYear = cursor1.getInt(1);
+                ComeMonth =cursor1.getInt(2);
+                ComeDay =cursor1.getInt(3);
+                ComeDate = ComeYear+"."+ComeMonth+"."+ComeDay;
+                bt1 = findViewById(R.id.bt1);
+                bt1.setText(ComeDate);
+                cursor1.close();
+            }
+
+            String SelectOutSQL = "select * from DDay where num=1";
+            String OutDate;
+            Cursor cursor2 = SQLitedb.rawQuery(SelectOutSQL, null);
+            if(cursor2.moveToFirst()) {
+                OutYear = cursor2.getInt(1);
+                OutMonth =cursor2.getInt(2);
+                OutDay =cursor2.getInt(3);
+                OutDate = OutYear+"."+OutMonth+"."+OutDay;
+                bt2 = findViewById(R.id.bt2);
+                bt2.setText(OutDate);
+                cursor2.close();
+            }
+        } catch (SQLiteException e){
+            Toast.makeText(getApplicationContext(), "can't get ComeDate", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void init_database(){
+
+        SQLitedb = null;
+        File file = new File(getFilesDir(), "SQLitedb.db");
+        try {
+            SQLitedb = SQLiteDatabase.openOrCreateDatabase(file, null); //이는 데이터베이스 직접 호출 방식, SQLiteOpenHelper를 통한 호출 방식이 좋다고 함
+            Toast.makeText(getApplicationContext(), "success create database! ",Toast.LENGTH_SHORT).show();
+        }catch (SQLiteException e){
+            Toast.makeText(getApplicationContext(), "can't not create database",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void init_table(){
+        if(SQLitedb != null) {
+            try {
+                String tableSQL = "Create table if not exists DDay " +
+                        "(num integer , " +
+                        "year integer, " +
+                        "month integer, " +
+                        "day integer)";
+                SQLitedb.execSQL(tableSQL);
+            }catch (SQLiteException e){
+                Toast.makeText(getApplicationContext(),"error, can't not create table",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void save_values(){
+
+        if(SQLitedb != null){
+            try {
+                //테이블에 존재하는 모든 튜플 삭제
+                SQLitedb.execSQL("Delete from DDay");
+                //입력한 데이터 저장
+                String ComeloadSQL = "insert into DDay (num, year, month, day) values ("+
+                        ""+'0'+",'"+ComeYear+"','"+ComeMonth+"','"+ComeDay+"')";
+                SQLitedb.execSQL(ComeloadSQL);
+
+                String OutloadSQL = "insert into DDay (num, year, month, day) values ("+
+                        ""+'1'+",'"+OutYear+"','"+OutMonth+"','"+OutDay+"')";
+                SQLitedb.execSQL(OutloadSQL);
+
+                Toast.makeText(getApplicationContext(), "success input values", Toast.LENGTH_SHORT).show();
+            }catch (SQLiteException e){
+                Toast.makeText(getApplicationContext(),"can't not insert values", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    //입대 데이트피커 입력
+    private DatePickerDialog.OnDateSetListener listener1 = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            ComeYear = year;
+            ComeMonth = (monthOfYear+1);
+            ComeDay = dayOfMonth;
+            bt1.setText(year+"."+(monthOfYear+1)+"."+ dayOfMonth);
+        }
+    };
+
+    //전역 데이트피커 입력
+    private DatePickerDialog.OnDateSetListener listener2 = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            OutYear = year;
+            OutMonth = (monthOfYear+1);
+            OutDay = dayOfMonth;
+            bt2.setText(year+"."+(monthOfYear+1)+"."+ dayOfMonth);
+        }
+    };
+}
